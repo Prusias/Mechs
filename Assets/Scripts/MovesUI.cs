2a01c27bbac347be4aea57cfaa210997397a6559
Prusias <MikeJordy1999@gmail.com>
2019-04-09 14:41:35 +0100

    2

---
+++ Assets/Scripts/MovesUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MechAndSandals;
+using MechAndSandals.Weapons;
+
+public class ActionsUI : MonoBehaviour {
+
+    public static GameObject buttonPrefab;
+    public static GameObject weaponButtonParent;
+    public List<GameObject> weaponButtons;
+    List<IWeapon> weapons;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        weapons.Add(new Gun(100, "GoodGun"));
+        weapons.Add(new Lazer(100, "GoodLaser"));
+
+        Transform transform = weaponButtonParent.transform;
+
+        int count = 0;
+        foreach (IWeapon weapon in weapons)
+        {
+            GameObject button = Instantiate(buttonPrefab, weaponButtonParent.transform);
+            button.GetComponent<Button>().onClick.AddListener(() => ButtonClicked(1));
+        }
+  
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+
+	}
+
+    void ButtonClicked(int buttonNo)
+    {
+        //Output this to console when the Button3 is clicked
+        Debug.Log("Button clicked = " + buttonNo);
+    }
+}
