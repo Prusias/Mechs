4c274e23c2742088a89db12a11c7df2de4e733bf
Prusias <MikeJordy1999@gmail.com>
2019-04-11 11:10:11 +0100

    I hate vim

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -9,29 +9,78 @@ namespace MechAndSandals
 
     {
         public GameObject playerGameObject;
+        public GameObject AIplayerGameObject;
         public GameObject textInfo;
         Player player;
+        Player AIplayer;
+        public Player currentPlayer; 
 
         // Use this for initialization
         void Start()
         {
             player = playerGameObject.GetComponent<Player>();
+            player = new Player(
+
+            );
+            AIplayer = AIplayerGameObject.GetComponent<Player>();
+            AIplayer = new Player(
+
+            );
             textInfo.GetComponent<Text>().text = "Your Turn";
+            currentPlayer = player;
         }
 
         // Update is called once per frame
         void Update()
         {
-            player.Health--;
+            
+
+
         }
 
         public void Cast(IAbility ability, Player player, bool endturn = true)
         {
-
+            ability.Cast(player);
+            EndTurn();
         }
+
         public void Attack(IWeapon weapon, int weaponType, Player player, bool endturn = true)
         {
+            if (weaponType == 1)
+            {
+                player.QuickAttack(GetOpponent());
+            } else if (weaponType == 2)
+            {
+                player.NormalAttack(GetOpponent());
+            } else if (weaponType == 3)
+            {
+                player.HeavyAttack(GetOpponent());
+            }
+            EndTurn(); 
+        }
 
+        private void EndTurn()
+        {
+            currentPlayer = GetOpponent();
+            if (currentPlayer == player)
+            {
+                textInfo.GetComponent<Text>().text = "Your Turn";
+            } else
+            {
+                textInfo.GetComponent<Text>().text = "Opponents Turn";
+            }
+
+        }
+
+        public Player GetOpponent()
+        {
+            if (currentPlayer == player)
+            {
+                return AIplayer;
+            } else
+            {
+                return player;
+            }
         }
     }
 
