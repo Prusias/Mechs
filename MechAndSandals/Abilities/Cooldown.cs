cd53b8633fe6131d0a8967deebbef6376000b05d
Short Course 080 <sc080>
2019-04-11 11:46:08 +0100

    cooldown

---
+++ MechAndSandals/Abilities/Cooldown.cs
@@ -0,0 +1,12 @@
+﻿namespace MechAndSandals.Abilities
+{
+    internal class Cooldown : IAbility
+    {
+        private int v;
+
+        public Cooldown(int v)
+        {
+            this.v = v;
+        }
+    }
+}
